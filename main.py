034332e5691bdbad4bbc191020256177aae61daa
Deep Kamal Gupta <deepkamalgupta13@gmail.com>
2026-03-21 16:42:48 +0530

    Main code

--- main.py
+++ main.py
@@ -1,6 +1,73 @@
-print("hello")
+MAX_LINES = 3
+MAX_BET = 1000
+MIN_BET = 1
 
-def add():
-  print(2 + 4)
-add()
- 
+def deposit():
+  
+  while True:
+    amount = input("What would you like to deposit $ ")
+    if amount.isdigit():
+      amount = int(amount)
+      
+      if amount > 0:
+          break
+      else:
+        print("Amount must be greater than 0")
+        
+    else:
+      print("Please enter a number")
+      
+  return amount
+
+
+def get_number_of_lines():
+    
+  while True:
+    lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
+    if lines.isdigit():
+      lines = int(lines)
+      
+      if lines > 0:
+          break
+      else:
+        print("Enter a Valid number of lines.  ")
+        
+    else:
+      print("Please enter a number")
+      
+  return lines
+
+def get_bet():
+    while True:
+      amount = input("What would you like to bet on each line? $ ")
+      if amount.isdigit():
+        amount = int(amount)
+
+        if MIN_BET <= amount <= MAX_BET:
+            break
+        else:
+          print(f"Amount must be between ${MIN_BET} - ${MAX_BET}.")
+
+      else:
+        print("Please enter a number")
+      
+    return amount
+
+def main():
+  balance = deposit()
+  lines = get_number_of_lines()
+  while True:
+    bet = get_bet()
+    total_bet = bet * lines
+    
+    if total_bet > balance:
+      print(f"NOT enought amount to bet, you current balance is ${balance}")
+    else:
+      break
+    
+  print(f"You are batting ${bet} on {lines} lines. your total bet is equals to ${total_bet}")
+  
+main()
+  
+  
+
